d60241eeaf45b0a5f3cfdf833f5d2427dce2237f
mohamedrishal <rishalmundekkat@gmail.com>
2024-02-26 22:44:46 +0530

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+// import video from '../../Assets/'
+
+
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="overlay">
+        <video src="" type='video/mp4'></video>
+      </div>
+    </section>
+  )
+}
+
+export default Home
