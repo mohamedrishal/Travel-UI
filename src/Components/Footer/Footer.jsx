e955bb845a409b1e52444614892a4c2c879475d1
mohamedrishal <rishalmundekkat@gmail.com>
2024-03-03 21:55:55 +0530

    done structure

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,190 @@
-import React from 'react'
+import React from "react";
+import video2 from "../../Assets/video2.mp4";
+import { FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { FiChevronRight } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className="footer">
+
+    <div className="videoDiv">
+      <video src={video2} loop autoPlay muted type="video/mp4"></video>
+    </div>
+
+    <div className="secContent container">
+
+      <div className="contactDiv flex">
+        <div className="text">
+          <small>KEEP IN TOUCH</small>
+          <h2>Travel with us</h2>
+        </div>
+        <div className="inputDiv flex">
+          <input type="text" placeholder="Enter Email Address" />
+          <button className="btn flex">
+            SEND
+            <FiSend className="icon" />
+          </button>
+        </div>
+      </div>
+
+      <div className="footerCard flex">
+        <div className="footerIntro flex">
+          
+          <div className="logoDiv">
+            <a href="" className="logo flex">
+              <MdOutlineTravelExplore className="icon" /> Travel.
+            </a>
+          </div>
+
+          <div className="footerParagraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+            molestias cumque consequuntur, voluptatem culpa ad quo enim
+            officia quaerat labore iste magnam omnis at. Suscipit, aut culpa!
+            Harum, iusto tenetur!
+          </div>
+
+          <div className="footerSocials flex">
+            <AiOutlineTwitter className="icon" />
+            <AiFillYoutube className="icon" />
+            <AiFillInstagram className="icon" />
+            <FaTripadvisor className="icon" />
+          </div>
+        </div>
+
+        <div className="footerLinks grid">
+          {/* Group One  */}
+          <div className="linkGroup">
+            <span className="groupTitle">OUR AGENCY</span>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Services
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Insurence
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Agency
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Tourism
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Payment
+            </li>
+          </div>
+
+          {/* Group One  */}
+          <div className="linkGroup">
+            <span className="groupTitle">PARTNERS</span>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Bookings
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Rentcars
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              HostelWorld
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              TripAdvisor
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Payment
+            </li>
+          </div>
+
+          {/* Group Two  */}
+          <div className="linkGroup">
+            <span className="groupTitle">PARTNERS</span>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Bookings
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Rentcars
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              HostelWorld
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              TripAdvisor
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Payment
+            </li>
+          </div>
+
+          {/* Group Three  */}
+          <div className="linkGroup">
+            <span className="groupTitle">LAST MINUTE</span>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              London
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              California
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Indonesia
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Europe
+            </li>
+
+            <li className="footerList flex">
+              <FiChevronRight className="icon" />
+              Oceania
+            </li>
+          </div>
+        </div>
+        <div className="footerDiv flex">
+          <small>BEST TRAVEL WEBSITE THEME</small>
+          <small>COPYRIGHTS RESERVED - WEBTEC 2024 </small>
+        </div>
+      </div>
+    </div>
+
+  </section>
   )
 }
 
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,18 +1,160 @@
 import React from 'react'
 import './main.css'
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
+import img1 from "../../Assets/img1.jpg"
+import img2 from "../../Assets/img2.jpg"
+import img3 from "../../Assets/img3.jpg"
+import img4 from "../../Assets/img4.jpg"
+import img5 from "../../Assets/img5.jpg"
+import img6 from "../../Assets/img6.jpg"
+import img7 from "../../Assets/img7.jpg"
+import img8 from "../../Assets/img8.jpg"
+import img9 from "../../Assets/img9.jpg"
+import img10 from "../../Assets/img10.jpg"
 
 const Main = () => {
+
+  const Data = [
+    {
+      id: 1,
+      imgSrc: img1,
+      destTitle: "Bore Bore",
+      location: "New Zealand",
+      grade: "CULTURAL RELAX",
+      fees: "$700",
+      destination: "The epitome of romance, Bore Bore is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities."
+    },
+    {
+      id: 2,
+      imgSrc: img2,
+      destTitle: "Mystic Mountains",
+      location: "Switzerland",
+      grade: "SCENIC BEAUTY",
+      fees: "$800",
+      destination: "Surrounded by breathtaking landscapes, Mystic Mountains offers a serene escape with its picturesque views and charming atmosphere."
+    },
+    {
+      id: 3,
+      imgSrc: img3,
+      destTitle: "Azure Beaches",
+      location: "Maldives",
+      grade: "TROPICAL PARADISE",
+      fees: "$900",
+      destination: "Experience the beauty of crystal-clear waters and white sandy beaches at Azure Beaches, a tropical paradise perfect for relaxation and water adventures."
+    },
+    {
+      id:4,
+      imgSrc: img4,
+      destTitle: "Enchanting Elysium",
+      location: "Greece",
+      grade: "MYTHICAL ESCAPE",
+      fees: "$750",
+      destination: "Immerse yourself in the ancient charm of Enchanting Elysium, where history, mythology, and breathtaking landscapes come together for an unforgettable experience."
+    },
+    {
+      id: 5,
+      imgSrc: img5,
+      destTitle: "Serenity Sands",
+      location: "Thailand",
+      grade: "TROPICAL TRANQUILITY",
+      fees: "$600",
+      destination: "Escape to the tranquility of Serenity Sands, where golden beaches, lush greenery, and vibrant culture create a perfect blend for a peaceful getaway."
+    },
+    {
+      id: 6,
+      imgSrc: img6,
+      destTitle: "Northern Lights Haven",
+      location: "Norway",
+      grade: "NATURAL WONDERS",
+      fees: "$950",
+      destination: "Witness the magical dance of the Northern Lights at Northern Lights Haven, a destination that combines natural wonders with cozy atmospheres and thrilling experiences."
+    },
+    {
+      id: 7,
+      imgSrc: img7,
+      destTitle: "Cultural Marvel",
+      location: "Japan",
+      grade: "CULTURAL ENRICHMENT",
+      fees: "$820",
+      destination: "Immerse yourself in the rich cultural tapestry of Japan at Cultural Marvel, where traditional beauty meets modern marvels in a harmonious blend."
+    },
+    {
+      id: 8,
+      imgSrc: img8,
+      destTitle: "Majestic Safari",
+      location: "Kenya",
+      grade: "WILDLIFE ADVENTURE",
+      fees: "$1200",
+      destination: "Embark on a majestic safari adventure at Majestic Safari, where the untamed beauty of African wildlife unfolds against the backdrop of breathtaking landscapes."
+    },
+    {
+      id: 9,
+      imgSrc: img9,
+      destTitle: "Rustic Retreat",
+      location: "Italy",
+      grade: "COUNTRYSIDE CHARM",
+      fees: "$680",
+      destination: "Experience the charm of the Italian countryside at Rustic Retreat, where rolling hills, vineyards, and historic villages create a perfect backdrop for relaxation."
+    },
+    {
+      id: 10,
+      imgSrc: img10,
+      destTitle: "Island Oasis",
+      location: "Fiji",
+      grade: "TROPICAL ESCAPE",
+      fees: "$850",
+      destination: "Indulge in a tropical paradise at Island Oasis, where pristine beaches, turquoise waters, and vibrant coral reefs offer an idyllic escape from the everyday."
+    }
+  ]
+
   return (
     <section className='main container section'>
      
-      <div className="secTi">
-        <h3 className="tit">
+      <div className="secTitle">
+        <h3 className="title">
           Most visited distinations
         </h3>
       </div>
 
       <div className="secContent grid">
-        
+        { Data.map(({id,imgSrc,destTitle,location,grade,fees,destination})=>{ return(
+          <div key={id} className='singleDestination'>
+            <div className="imageDiv">
+              <img src={imgSrc} alt={destTitle} />
+            </div>
+
+            <div className="cardInfo">
+
+              <h4 className="destTitle">{destTitle}</h4>
+
+              <span className="continent flex">
+                <HiOutlineLocationMarker className='icon'/>
+                <span className="name">
+                  {location}
+                </span>
+              </span>
+
+              <div className="fees flex">
+                <div className="grade">
+                  <span>{grade}<small>+1</small></span>
+                </div>
+                <div className="price">
+                  <h5>{fees}</h5>
+                </div>
+              </div>
+
+              <div className="desc">
+                <p>{destination}</p>
+              </div>
+
+              <button className='btn flex'>
+                DETAILS <HiOutlineClipboardCheck className='icon'/>
+              </button>
+
+            </div>
+          </div>
+        )})}
       </div>
 
     </section>
